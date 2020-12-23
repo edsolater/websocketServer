7e5dd036d18e97e1242d6628e04e663aab18e3fa
edsolater <edsolater@qq.com>
2020-12-23 23:32:58 +0800

    还需要一个生成新的RTC通道的命令

--- websocketServer.ts
+++ websocketServer.ts
@@ -1,3 +1,4 @@
+/**TODO: 目前只是1对多，会议要多对多 */
 import * as WebSocket from "ws"
 import {
   sendToClient,
@@ -7,23 +8,36 @@ import {
 import { ID } from "./type"
 import { creatId } from "./uuid"
 
-const rooms: {
-  [roomId in ID]: {
-    talker: ID
-    connectedWebsockets: Map<ID, WebSocket> // 每个人有且只有一个websocket
-    userIdentity: Map<ID, 'TALKER' | 'AUDIENCE'>
-    peers: Set<[ID, ID]> //只有观众能产生peer
-  }
-} = {}
+type RoomInfo = {
+  talker: ID
+  audiences: ID[]
+  connectedWebsockets: Map<ID, WebSocket> // 每个人有且只有一个websocket
+  userIdentity: Map<ID, "TALKER" | "AUDIENCE">
+  peers: Set<[ID, ID]> //只有观众能产生peer
+}
+
+const rooms: { [roomId in ID]: RoomInfo } = {
+  "1": {
+    talker: "",
+    audiences: [],
+    connectedWebsockets: new Map(),
+    userIdentity: new Map(),
+    peers: new Set(),
+  },
+}
 const wss = new WebSocket.Server({ port: 5000 })
 wss.on("connection", initConnect)
 
 function initConnect(ws: WebSocket) {
+  /**这条链接的所有者的id */
   let userId: ID = creatId()
-  let peerId: ID
   // const roomId = urlHasRoomId ?  "1" // TODO: rommID 可能由前端传过来，也可能随机生成一个
-  let roomId: ID = "1"
-  rooms[roomId].connectedWebsockets.set(userId, ws)
+  /**这条链接所处的房间号的id */
+  let roomId: ID = "1" //TEMP 暂时写死
+
+  rooms[roomId].connectedWebsockets.set(userId, ws) //TODO: 这里能取到这个属性，这个假设是不对的
+
+  // 如果客户端断开
   ws.on("close", () => {
     rooms[roomId].connectedWebsockets.delete(userId)
     rooms[roomId].userIdentity.delete(userId)
@@ -34,66 +48,94 @@ function initConnect(ws: WebSocket) {
       rooms[roomId].peers.delete(peer)
     })
   })
-  // 如果是第一个进入的，那TA就是主播，不然就是主播
-  if (rooms[roomId].connectedWebsockets.size === 1 /* 此规则是暂时的 */) {
-    console.log("a TALKER has come")
-    rooms[roomId].userIdentity.set(userId, "TALKER")
-    rooms[roomId].talker = userId
-    sendToClient(ws, "ID", { userId, roomId, content: "TALKER" })
-  } else {
-    console.log("an AUDIENCE has come")
-    rooms[roomId].userIdentity.set(userId, "AUDIENCE")
-    peerId = rooms[roomId].talker
-    rooms[roomId].peers.add([peerId, userId])
-    sendToClient(ws, "ID", { userId, roomId, content: "AUDIENCE" })
-  }
   ws.on("message", (message) =>
     handleClientMessage({
       ws,
-      jsonMessage: message,
       userId,
-      userIdentity: rooms[roomId].userIdentity.get(userId),
       roomId,
+      jsonMessage: message,
+      roomInfo: rooms[roomId],
     })
   )
 }
 
 function handleClientMessage({
   ws,
-  jsonMessage,
   userId,
   roomId,
-  userIdentity,
+  jsonMessage,
+  roomInfo,
 }: {
   ws: WebSocket
-  jsonMessage: WebSocket.Data
   userId: ID
   roomId: ID
-  userIdentity: "TALKER" | "AUDIENCE"
+  jsonMessage: WebSocket.Data
+  roomInfo: RoomInfo
 }) {
   if (typeof jsonMessage !== "string") return
   const command = getCommandFromClient(jsonMessage)
   switch (command) {
+    case "JOIN": {
+      // 如果此用户是第一个进入的，那TA就是主播，否则是观众
+      if (roomInfo.connectedWebsockets.size === 1 /* 此规则是暂时的 */) {
+        console.log("a TALKER has come")
+        roomInfo.userIdentity.set(userId, "TALKER")
+        roomInfo.talker = userId
+        sendToClient(ws, "ID", {
+          peerId: "",
+          userId,
+          roomId,
+          content: "TALKER",
+        })
+      } else {
+        console.log("an AUDIENCE has come")
+        const talkerId = roomInfo.talker
+        const talkerWS = roomInfo.connectedWebsockets.get(talkerId)
+        roomInfo.userIdentity.set(userId, "AUDIENCE")
+        roomInfo.audiences.push(userId)
+        roomInfo.peers.add([talkerId, userId])
+        sendToClient(ws, "ID", {
+          peerId: talkerId,
+          userId,
+          roomId,
+          content: "AUDIENCE",
+        })
+        sendToClient(talkerWS, "CAN_OFFER", {
+          peerId: userId,
+          userId: talkerId,
+          roomId,
+        })
+      }
+      break
+    }
     case "CREATE_OFFER": {
       console.log("主播发来offer")
       const payload = getPayloadFromClient<typeof command>(jsonMessage)
-      sendToClient(ws, "RECEIVE_OFFER", { content: payload.content })
+      const peerId = payload.peerId
+      const peerWS = roomInfo.connectedWebsockets.get(peerId)
+      sendToClient(peerWS, "RECEIVE_OFFER", {
+        peerId,
+        content: payload.content,
+      })
       break
     }
     case "CREATE_ANSWER": {
       console.log("观众发来answer")
       const payload = getPayloadFromClient<typeof command>(jsonMessage)
-      sendToClient(ws, "RECEIVE_ANSWER", { content: payload.content })
+      const peerId = payload.peerId
+      const peerWS = roomInfo.connectedWebsockets.get(peerId)
+      sendToClient(peerWS, "RECEIVE_ANSWER", {
+        peerId,
+        content: payload.content,
+      })
       break
     }
     case "CANDIDATE": {
       console.log("主播/观众发来candidate")
       const payload = getPayloadFromClient<typeof command>(jsonMessage)
-      sendToClient(
-        rooms[roomId].connectedWebsockets.get(talkerId),
-        "CANDIDATE",
-        { content: payload.content }
-      )
+      const peerId = payload.peerId
+      const peerWS = roomInfo.connectedWebsockets.get(peerId)
+      sendToClient(peerWS, "CANDIDATE", { peerId, content: payload.content })
       break
     }
     default:
--- websocketTransformMethods.ts
+++ websocketTransformMethods.ts
@@ -7,15 +7,49 @@ type WebRTCIdentity = "TALKER" | "AUDIENCE"
 type Commands = {
   //TODO: 感觉这个流程不太对， P2P不应该是有方向的。offer不可知的话，这个流程有点怪异
   JOIN: {} // 当用户进入时，告知后端
-  ID: { userId: string; roomId: string; content: WebRTCIdentity } // 后端告知身份信息
-
-  CAN_OFFER: {} //【主播接收】后端通知主播：可以发来OFFER了 // 仅限主播在第二个人加入后，收到
-  CREATE_OFFER: { content: RTCSessionDescriptionInit; userId: ID; roomId: ID } //【主播发起】主播给后端offer信息
-  RECEIVE_OFFER: { content: RTCSessionDescriptionInit; userId: ID; roomId: ID } //【观众接收】观众从后端收到主播的offer信息
-  CREATE_ANSWER: { content: RTCSessionDescriptionInit; userId: ID; roomId: ID } //【观众发起】观众给后端answer信息
-  RECEIVE_ANSWER: { content: RTCSessionDescriptionInit; userId: ID; roomId: ID } // 【主播接收】主播从后端收到观众的answer信息
-
-  CANDIDATE: { content: RTCIceCandidate; userId: ID; roomId: ID }
+  ID: {
+    peerId: ID
+    userId: ID
+    roomId: ID
+    content: WebRTCIdentity
+  } // 后端告知身份信息
+  //【主播接收】后端通知主播：可以发来OFFER了 // 仅限主播在第二个人加入后，才会收到
+  CAN_OFFER: { /* 需要建立连接的客户端 */ peerId: ID; userId: ID; roomId: ID }
+  //【主播发起】主播给后端offer信息
+  CREATE_OFFER: {
+    content: RTCSessionDescriptionInit
+    userId: ID
+    roomId: ID
+    peerId: ID
+  }
+  //【观众接收】观众从后端收到主播的offer信息
+  RECEIVE_OFFER: {
+    content: RTCSessionDescriptionInit
+    userId: ID
+    roomId: ID
+    peerId: ID
+  }
+  //【观众发起】观众给后端answer信息
+  CREATE_ANSWER: {
+    content: RTCSessionDescriptionInit
+    userId: ID
+    roomId: ID
+    peerId: ID
+  }
+  // 【主播接收】主播从后端收到观众的answer信息
+  RECEIVE_ANSWER: {
+    content: RTCSessionDescriptionInit
+    userId: ID
+    roomId: ID
+    peerId: ID
+  }
+  // 主播观众通用
+  CANDIDATE: {
+    content: RTCIceCandidate
+    userId: ID
+    roomId: ID
+    peerId: ID
+  }
 }
 /**
  * 脏函数，发送websocket信息给客户端
